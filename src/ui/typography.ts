c3189f6f1e4dbe8ef0bd22da680b23550c59e656
Zlata <zlatasaburova97@gmail.com>
2022-05-10 11:59:02 +0300

    feat(ui): add typography

---
+++ src/ui/typography.ts
@@ -0,0 +1,92 @@
+import { css } from "styled-components";
+import { media } from "./media";
+
+const H1 = css`
+  font-weight: 700;
+  font-size: 56px;
+  line-height: 64px;
+  color: rgb(49, 48, 55);
+
+  ${media.SM} {
+    font-size: 32px;
+    line-height: 44px;
+  }
+`;
+
+const H2 = css`
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 60px;
+  color: rgb(49, 48, 55);
+
+  ${media.SM} {
+    font-size: 28px;
+    line-height: 40px;
+  }
+`;
+
+const H3 = css`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 32px;
+  color: rgb(49, 48, 55);
+`;
+
+const subtitle = css`
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 24px;
+  color: rgb(49, 48, 55);
+`;
+
+const bodytext = css`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 20px;
+  color: rgb(49, 48, 55);
+`;
+
+const button = css`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 24px;
+`;
+
+const input = css`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 32px;
+  color: rgb(168, 168, 168);
+
+  &::placeholder {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 32px;
+    color: rgb(168, 168, 168);
+  }
+`;
+
+const link = css`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: rgb(49, 48, 55);
+`;
+
+const tab = css`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export const typography = {
+  H1,
+  H2,
+  H3,
+  subtitle,
+  bodytext,
+  button,
+  input,
+  link,
+  tab,
+};
