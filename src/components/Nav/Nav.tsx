992bd41fefadca93281b473a0ce89f023b8bc6af
Zlata <zlatasaburova97@gmail.com>
2022-05-13 15:34:09 +0300

    refactor(nav): change input

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import likes from "../../assets/svg/likes.svg";
 import cart from "../../assets/svg/cart.svg";
-import account from "../../assets/svg/account..svg";
+import account from "../../assets/svg/account.svg";
 import { StyledIcon, StyledNav } from "./styles";
 
 export const Nav = () => {
