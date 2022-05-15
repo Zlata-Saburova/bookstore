26560b8b773fd6b16f2fa1d87a13625e4556e728
Zlata <zlatasaburova97@gmail.com>
2022-05-16 00:05:15 +0300

    fix(list): change grid gap

--- src/components/List/styles.ts
+++ src/components/List/styles.ts
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const StyledList = styled.ul`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(352px, 1fr));
-  grid-gap: 20px;
+  column-gap: 32px;
+  row-gap: 49px;
 
   max-width: 1120px;
   width: 100%;
