fb81fceccd57dc7daad5fa76b8d3df0c1b626da9
Zlata <zlatasaburova97@gmail.com>
2022-05-15 22:21:45 +0300

    feat(app router): add pages

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,10 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import { MainTemplate } from "../components/MainTemplate";
+import { Home } from "../pages/Home";
+import { NewBooks } from "../pages/NewBooks";
+import { Search } from "../pages/Search";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/bookstore" element={<MainTemplate />} />
+      <Route path="/bookstore" element={<MainTemplate />}>
+        <Route path="/bookstore" element={<Home />} />
+        <Route path="/bookstore/new" element={<NewBooks />} />
+        <Route path="search" element={<Search />} />
+      </Route>
     </Routes>
   );
 };
