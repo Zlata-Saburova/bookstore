bd5a878eb78f7ac42d835bd0cbe815e7b2df3ab8
Zlata <zlatasaburova97@gmail.com>
2022-05-16 00:35:50 +0300

    refactor(header): remove container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,18 +2,16 @@ import { Link } from "react-router-dom";
 import logo from "../../assets/svg/logo.svg";
 import { HeaderForm } from "../HeaderForm/HeaderForm";
 import { Nav } from "../Nav/Nav";
-import { StyledHeader, HeaderContainer, Logo } from "./styles";
+import { StyledHeader, Logo } from "./styles";
 
 export const Header = () => {
   return (
     <StyledHeader>
-      <HeaderContainer>
-        <Link to="/bookstore">
-          <Logo src={logo} alt="logo" />
-        </Link>
-        <HeaderForm />
-        <Nav />
-      </HeaderContainer>
+      <Link to="/bookstore">
+        <Logo src={logo} alt="logo" />
+      </Link>
+      <HeaderForm />
+      <Nav />
     </StyledHeader>
   );
 };
