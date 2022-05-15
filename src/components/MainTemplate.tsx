87a7513b2bb5794a11a8cf834061dfb8766ea04a
Zlata <zlatasaburova97@gmail.com>
2022-05-16 00:35:31 +0300

    feat(main template): add outlet and footer

--- src/components/MainTemplate.tsx
+++ src/components/MainTemplate.tsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom";
+import { Footer } from "../Footer/Footer";
 import { Header } from "./Header/Header";
 
 export const MainTemplate = () => {
   return (
     <>
       <Header />
+      <Outlet />
+      <Footer />
     </>
   );
 };
