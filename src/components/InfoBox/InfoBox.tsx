07e2a40d6fee6310513dea2f26ba253396fa87a3
Zlata <zlatasaburova97@gmail.com>
2022-05-16 23:49:31 +0300

    feat(info box): add new component and style it

---
+++ src/components/InfoBox/InfoBox.tsx
@@ -0,0 +1,35 @@
+import { useForm } from "react-hook-form";
+import {
+  BoxButton,
+  BoxDescription,
+  BoxTitle,
+  StyledForm,
+  StyledInfoBox,
+  StyledInput,
+} from "./styles";
+
+type FormValues = {
+  email: string;
+};
+
+export const InfoBox = () => {
+  const { register } = useForm<FormValues>();
+  return (
+    <StyledInfoBox>
+      <BoxTitle>Subscribe to Newsletter</BoxTitle>
+      <BoxDescription>
+        Be the first to know about new IT books, upcoming releases, exclusive
+        offers and more.
+      </BoxDescription>
+      <StyledForm>
+        <StyledInput
+          placeholder="Your email"
+          {...register("email", {
+            required: true,
+          })}
+        ></StyledInput>
+        <BoxButton whileHover={{ scale: 1.05 }}>Subscribe</BoxButton>
+      </StyledForm>
+    </StyledInfoBox>
+  );
+};
---
+++ src/components/InfoBox/styles.ts
@@ -0,0 +1,58 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import { typography } from "../../ui/typography";
+
+export const StyledInfoBox = styled.div`
+  padding: 56px 64px;
+  margin-top: 72px;
+
+  background-color: rgb(241, 218, 196);
+`;
+
+export const BoxTitle = styled.h3`
+  margin-bottom: 32px;
+
+  ${typography.H3}
+  color: rgb(13, 12, 29);
+`;
+
+export const BoxDescription = styled.p`
+  margin-bottom: 32px;
+
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 28px;
+  color: rgb(168, 168, 168);
+`;
+
+export const StyledForm = styled.form`
+  display: flex;
+
+  max-width: 992px;
+  width: 100%;
+`;
+
+export const StyledInput = styled.input`
+  max-width: 845px;
+  width: 100%;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  padding-left: 24px;
+
+  background-color: rgb(255, 255, 255);
+
+  ${typography.input}
+`;
+
+export const BoxButton = styled(motion.button)`
+  padding: 16px 40px;
+
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 24px;
+  color: rgb(255, 255, 255);
+
+  background-color: rgb(13, 12, 29);
+
+  cursor: pointer;
+`;
