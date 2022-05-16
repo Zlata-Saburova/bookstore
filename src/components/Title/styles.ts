b88635606eef0a665e0ee895d8ce4b7bcbd8d49e
Zlata <zlatasaburova97@gmail.com>
2022-05-16 16:03:12 +0300

    feat(title): add new component and style it

---
+++ src/components/Title/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { typography } from "../../ui/typography";
+
+export const StyledTitle = styled.h1`
+  margin-bottom: 48px;
+  text-align: left;
+
+  ${typography.H1};
+`;
