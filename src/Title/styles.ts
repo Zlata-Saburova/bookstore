ecc22413ffc04af648f10cd83338840f616d8f88
Zlata <zlatasaburova97@gmail.com>
2022-05-16 16:06:01 +0300

    refactor(title): replace component

--- src/Title/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { typography } from "../ui/typography";
-
-export const StyledTitle = styled.h1`
-  margin-bottom: 48px;
-  text-align: left;
-
-  ${typography.H1};
-`;
