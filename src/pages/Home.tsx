65cac7ed8ee608c1713c23f4820538bc517d0bab
Zlata <zlatasaburova97@gmail.com>
2022-05-16 16:03:45 +0300

    feat(home): add slider to the page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { BookSlider } from "../components/BookSlider/BookSlider";
+import { bookApi } from "../services/bookService";
+import { INewBooksApi } from "../services/types";
 
 export const Home = () => {
+  const [newBooks, setNewBooks] = useState<INewBooksApi>({
+    books: [],
+    error: "",
+    total: "",
+  });
+
+  useEffect(() => {
+    bookApi.getNewBooks().then((books) => {
+      setNewBooks(books);
+    });
+  }, []);
   return (
     <>
-      <div>Home</div>
-      <Link to="/bookstore/new">Watch new books</Link>
+      <NewLink to="/bookstore/new">
+        <BookSlider books={newBooks.books} />
+      </NewLink>
     </>
   );
 };
+
+const NewLink = styled(Link)`
+  text-decoration: none;
+`;
