376a6128d4a1fd52c0987e8179343a43220e9065
Zlata <zlatasaburova97@gmail.com>
2022-05-18 00:58:50 +0300

    feat(list item): add link

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 import { INewBookApi } from "../../services/types";
 import {
   StyledBookSubtitle,
@@ -13,10 +15,18 @@ interface IProps {
 export const ListItem = ({ book }: IProps) => {
   return (
     <StyledListItem whileHover={{ scale: 1.05 }}>
-      <img src={book.image} alt={book.title} />
-      <StyledBookTitle>{book.title}</StyledBookTitle>
-      <StyledBookSubtitle>{book.subtitle}</StyledBookSubtitle>
-      <StyledPrice>{book.price === "$0.00" ? "Free" : book.price}</StyledPrice>
+      <StyledLink to={`/bookstore/books/${book.isbn13}`}>
+        <img src={book.image} alt={book.title} />
+        <StyledBookTitle>{book.title}</StyledBookTitle>
+        <StyledBookSubtitle>{book.subtitle}</StyledBookSubtitle>
+        <StyledPrice>
+          {book.price === "$0.00" ? "Free" : book.price}
+        </StyledPrice>
+      </StyledLink>
     </StyledListItem>
   );
 };
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
