0355eed319c0243e27e197491ce548820095c886
Zlata <zlatasaburova97@gmail.com>
2022-05-18 00:58:28 +0300

    feat(pages): add new page for book details

---
+++ src/pages/BookDetails.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Book } from "../Book/Book";
+import { BackButton } from "../components/BackButton/BackButton";
+import { Title } from "../components/Title/Title";
+import { bookApi } from "../services/bookService";
+import { IBookDetailsApi } from "../services/types";
+
+export const BookDetails = () => {
+  const { id = "" } = useParams();
+  const [detailsBook, setDetailsBook] = useState<IBookDetailsApi>({
+    authors: "",
+    desc: "",
+    error: "",
+    image: "",
+    isbn10: "",
+    isbn13: "",
+    language: "",
+    pages: "",
+    pdf: {},
+    price: "",
+    publisher: "",
+    rating: "",
+    subtitle: "",
+    title: "",
+    url: "",
+    year: "",
+  });
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  useEffect(() => {
+    bookApi.getBookDetails(id).then((book) => {
+      setDetailsBook(book);
+    });
+  }, [id]);
+
+  return (
+    <>
+      <BackButton onClick={handleBack} />
+      <Title>{detailsBook.title}</Title>
+      <Book book={detailsBook} />
+    </>
+  );
+};
