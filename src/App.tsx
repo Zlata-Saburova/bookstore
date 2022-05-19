09e2b3818eaecfc2c565a3074b53eba01fbc3d91
Zlata <zlatasaburova97@gmail.com>
2022-05-19 23:35:53 +0300

    fix(app): remove styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,5 @@
-import styled from "styled-components";
-import GlobalStyles from "./GlobalStyles";
 import { AppRouter } from "./router/AppRouter";
 
 export const App = () => {
-  return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Container>
-          <AppRouter />
-        </Container>
-      </StyledApp>
-    </>
-  );
+  return <AppRouter />;
 };
-
-const StyledApp = styled.div`
-  padding: 0 16px;
-  max-width: 1920px;
-  width: 100%;
-`;
-
-const Container = styled.div`
-  max-width: 1120px;
-  width: 100%;
-  margin: 0 auto;
-`;
