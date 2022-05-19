a78ee89c0ae17bbcdead6d8c5c1ec45799c8c79a
Zlata <zlatasaburova97@gmail.com>
2022-05-19 23:36:37 +0300

    feat(main template): add styles

--- src/components/MainTemplate.tsx
+++ src/components/MainTemplate.tsx
@@ -1,13 +1,32 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import GlobalStyles from "../GlobalStyles";
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
 
 export const MainTemplate = () => {
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      <GlobalStyles />
+      <StyledApp>
+        <Container>
+          <Header />
+          <Outlet />
+          <Footer />
+        </Container>
+      </StyledApp>
     </>
   );
 };
+
+const StyledApp = styled.div`
+  padding: 0 16px;
+  max-width: 1920px;
+  width: 100%;
+`;
+
+const Container = styled.div`
+  max-width: 1120px;
+  width: 100%;
+  margin: 0 auto;
+`;
