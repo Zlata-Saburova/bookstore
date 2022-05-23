de07f478fb69b8f2cd330dead61f67f412f8e873
Zlata <zlatasaburova97@gmail.com>
2022-05-23 17:09:45 +0300

    feat(assets): add fole for importing all icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,25 @@
+import { ReactComponent as Account } from "./account.svg";
+import { ReactComponent as Back } from "./back.svg";
+import { ReactComponent as Cart } from "./cart.svg";
+import { ReactComponent as DarkStar } from "./dark-star.svg";
+import { ReactComponent as Details } from "./details.svg";
+import { ReactComponent as Facebook } from "./facebook.svg";
+import { ReactComponent as LightStar } from "./light-star.svg";
+import { ReactComponent as Likes } from "./likes.svg";
+import { ReactComponent as Logo } from "./logo.svg";
+import { ReactComponent as Search } from "./search.svg";
+import { ReactComponent as Twitter } from "./twitter.svg";
+
+export {
+  Account,
+  Back,
+  Cart,
+  DarkStar,
+  Details,
+  Facebook,
+  LightStar,
+  Likes,
+  Logo,
+  Search,
+  Twitter,
+};
