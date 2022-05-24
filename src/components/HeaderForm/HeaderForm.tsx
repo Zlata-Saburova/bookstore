a680b23a32f1779aea4f2dacdcff22b11c0c0915
Zlata <zlatasaburova97@gmail.com>
2022-05-25 00:19:50 +0300

    feat(header form): add novigation

--- src/components/HeaderForm/HeaderForm.tsx
+++ src/components/HeaderForm/HeaderForm.tsx
@@ -1,24 +1,22 @@
 import { FindButton, SearchInput, StyledHeaderForm } from "./styles";
 import { useForm } from "react-hook-form";
 import { Search } from "../../assets/icons";
-import { Link } from "react-router-dom";
-import { routes } from "../../routes/routes";
+import { useNavigate } from "react-router-dom";
 
 export const HeaderForm = () => {
   const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
 
   const onSubmit = (data: any) => {
-    console.log(data.title);
+    navigate(`/bookstore/search/${data.title}/1`);
   };
 
   return (
     <StyledHeaderForm onSubmit={handleSubmit(onSubmit)}>
       <SearchInput type="text" placeholder="Search" {...register("title")} />
-      <Link to={routes.SEARCH}>
-        <FindButton type="submit">
-          <Search />
-        </FindButton>
-      </Link>
+      <FindButton type="submit">
+        <Search />
+      </FindButton>
     </StyledHeaderForm>
   );
 };
