10ee9fee22f4d0952664cde7334f110e5825ae00
Zlata <zlatasaburova97@gmail.com>
2022-05-25 15:08:21 +0300

    feat(header form): add drop down search

--- src/components/HeaderForm/HeaderForm.tsx
+++ src/components/HeaderForm/HeaderForm.tsx
@@ -1,22 +1,78 @@
-import { FindButton, SearchInput, StyledHeaderForm } from "./styles";
+import {
+  Container,
+  FindButton,
+  Img,
+  ResultButton,
+  ResultLI,
+  ResultsBlock,
+  ResultTitle,
+  SearchInput,
+  StyledHeaderForm,
+  StyledLink,
+} from "./styles";
 import { useForm } from "react-hook-form";
 import { Search } from "../../assets/icons";
 import { useNavigate } from "react-router-dom";
+import { ChangeEvent, useEffect, useState } from "react";
+import { bookApi } from "../../services/bookService";
+import { ISearchBooksApi } from "../../services/types";
 
 export const HeaderForm = () => {
   const { register, handleSubmit } = useForm();
   const navigate = useNavigate();
 
+  const [title, setTitle] = useState("");
+
   const onSubmit = (data: any) => {
     navigate(`/bookstore/search/${data.title}/1`);
+    setTitle("");
   };
+
+  const [searchResult, setSearchResult] = useState<ISearchBooksApi>();
+
+  useEffect(() => {
+    bookApi.getBooksBySearch(title, "1").then((books) => {
+      setSearchResult(books);
+      console.log(books);
+    });
+  }, [title]);
 
   return (
     <StyledHeaderForm onSubmit={handleSubmit(onSubmit)}>
-      <SearchInput type="text" placeholder="Search" {...register("title")} />
+      <SearchInput
+        type="text"
+        placeholder="Search"
+        {...register("title", {
+          onChange: (e: ChangeEvent<HTMLInputElement>) =>
+            setTitle(e.target.value),
+        })}
+        value={title}
+      />
       <FindButton type="submit">
         <Search />
       </FindButton>
+      {title.length > 0 && (
+        <ResultsBlock>
+          <Container>
+            <>
+              {searchResult?.books.map((book) => {
+                return (
+                  <StyledLink
+                    to={`/bookstore/books/${book.isbn13}`}
+                    onClick={() => setTitle("")}
+                  >
+                    <ResultLI>
+                      <Img src={book.image} alt={book.title} />
+                      <ResultTitle>{book.title}</ResultTitle>
+                    </ResultLI>
+                  </StyledLink>
+                );
+              })}
+            </>
+            <ResultButton>all results</ResultButton>
+          </Container>
+        </ResultsBlock>
+      )}
     </StyledHeaderForm>
   );
 };
--- src/components/HeaderForm/styles.ts
+++ src/components/HeaderForm/styles.ts
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { typography } from "../../ui/typography";
 
@@ -30,4 +31,60 @@ export const FindButton = styled.button`
   height: 24px;
 
   cursor: pointer;
+`;
+
+export const ResultsBlock = styled.ul`
+  position: absolute;
+  top: 100%;
+  z-index: 3;
+
+  height: 400px;
+  overflow: auto;
+  width: 100%;
+  border: 1px solid rgb(231, 231, 231);
+  background-color: white;
+`;
+
+export const Container = styled.div`
+  position: relative;
+`;
+
+export const ResultLI = styled.li`
+  display: flex;
+  justify-content: left;
+  align-items: center;
+
+  height: 100px;
+  width: 100%;
+
+  border-bottom: 1px solid rgb(231, 231, 231);
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+export const ResultTitle = styled.p`
+  ${typography.body}
+`;
+
+export const Img = styled.img`
+  height: 90px;
+  margin-right: 20px;
+`;
+
+export const ResultButton = styled.button`
+  position: absolute;
+  top: 100%;
+  width: 100%;
+  padding-top: 17px;
+  padding-bottom: 17px;
+
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+
+  border-top: 1px solid rgb(231, 231, 231);
+  background-color: white;
 `;
