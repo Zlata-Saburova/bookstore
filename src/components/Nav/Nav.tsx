fc84fa92e2275847adc2ad2299d4df3d050223d9
Zlata <zlatasaburova97@gmail.com>
2022-05-27 00:13:45 +0300

    feat(nav): add link

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import { StyledIcon, StyledNav } from "./styles";
 import { Account, Cart, Likes } from "../../assets/icons";
+import { routes } from "../../routes/routes";
 
 export const Nav = () => {
   return (
     <StyledNav>
-      <Link to="">
+      <Link to={routes.FAVORITES}>
         <StyledIcon>
           <Likes />
         </StyledIcon>
