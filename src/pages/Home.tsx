228a2a9e8e3909e12d08de20e203fd1e9573b93a
Zlata <zlatasaburova97@gmail.com>
2022-05-26 12:42:06 +0300

    feat(home): add title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { BookSlider } from "../components/BookSlider/BookSlider";
 import { InfoBox } from "../components/InfoBox/InfoBox";
+import { Title } from "../components/Title/Title";
 import { bookApi } from "../services/bookService";
 import { INewBooksApi } from "../services/types";
 
@@ -20,6 +21,7 @@ export const Home = () => {
   }, []);
   return (
     <>
+      <Title>Don't miss our new books</Title>
       <NewLink to="/bookstore/new">
         <BookSlider books={newBooks.books} />
       </NewLink>
