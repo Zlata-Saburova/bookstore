087373a1f371f01f396c2bc55828c2a507ff9efb
Zlata <zlatasaburova97@gmail.com>
2022-05-27 00:10:55 +0300

    feat(routes): add new routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,4 +3,8 @@ export enum routes {
   NEW = "/bookstore/new",
   SEARCHRESULTS = "/bookstore/search/:title/:page",
   DETAILS = "/bookstore/books/:id",
+  SIGN_IN = "/bookstore/sign-in",
+  SIGN_UP = "/bookstore/sign-up",
+  NOT_FOUND = "*",
+  FAVORITES = "/bookstore/favorites",
 }
