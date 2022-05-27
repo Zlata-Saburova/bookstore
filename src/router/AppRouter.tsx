6eb078b84502c3a63922eff1f9e1b2c5323f78d7
Zlata <zlatasaburova97@gmail.com>
2022-05-28 00:31:09 +0300

    feat(router): add routes

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import { MainTemplate } from "../components/MainTemplate/MainTemplate";
 import { SearchResults } from "../components/SearchResults/SearchResults";
+import { Account } from "../pages/Account";
 import { BookDetails } from "../pages/BookDetails";
+import { Cart } from "../pages/Cart";
 import { Favorites } from "../pages/Favorites";
 import { Home } from "../pages/Home";
 import { NewBooks } from "../pages/NewBooks";
@@ -18,6 +20,8 @@ export const AppRouter = () => {
         <Route path={routes.DETAILS} element={<BookDetails />} />
         <Route path={routes.SIGN_IN} element={<SignIn />} />
         <Route path={routes.FAVORITES} element={<Favorites />} />
+        <Route path={routes.CART} element={<Cart />} />
+        <Route path={routes.ACCOUNT} element={<Account />} />
       </Route>
     </Routes>
   );
