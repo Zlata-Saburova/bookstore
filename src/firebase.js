c6f4cd52008ac63ceb288e1f0ac8a391440203ef
Zlata <zlatasaburova97@gmail.com>
2022-05-28 00:27:00 +0300

    install firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA71vyZ-6EtltjSC-mnFvaTuLe_Fsjk8mM",
+  authDomain: "bookstore-f8aee.firebaseapp.com",
+  projectId: "bookstore-f8aee",
+  storageBucket: "bookstore-f8aee.appspot.com",
+  messagingSenderId: "684161126673",
+  appId: "1:684161126673:web:0a11f627cb91762a83fcb0",
+};
+
+// Initialize Firebase
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
