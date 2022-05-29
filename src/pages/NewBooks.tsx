9a41cbcbc8a17eefbe7e9279d6e45323ce4e38e9
Zlata <zlatasaburova97@gmail.com>
2022-05-30 01:09:47 +0300

    feat(new books): add use of useSelector

--- src/pages/NewBooks.tsx
+++ src/pages/NewBooks.tsx
@@ -1,26 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { List } from "../components/List/List";
-import { bookApi } from "../services/bookService";
-import { INewBooksApi } from "../services/types";
+// import { bookApi } from "../services/bookService";
+// import { INewBooksApi } from "../services/types";
 import { Title } from "../components/Title/Title";
 import { InfoBox } from "../components/InfoBox/InfoBox";
+import { useSelector } from "react-redux";
+import { getBooks } from "../store/selectors/booksSelectors";
 
 export const NewBooks = () => {
-  const [newBooks, setNewBooks] = useState<INewBooksApi>({
-    books: [],
-    error: "",
-    total: "",
-  });
+  // const [newBooks, setNewBooks] = useState<INewBooksApi>({
+  //   books: [],
+  //   error: "",
+  //   total: "",
+  // });
 
-  useEffect(() => {
-    bookApi.getNewBooks().then((books) => {
-      setNewBooks(books);
-    });
-  }, []);
+  const books = useSelector(getBooks);
+
+  // useEffect(() => {
+  //   bookApi.getNewBooks().then((books) => {
+  //     setNewBooks(books);
+  //   });
+  // }, []);
+
   return (
     <>
       <Title>New Releases Books</Title>
-      <List books={newBooks.books} />
+      <List books={books} />
       <InfoBox />
     </>
   );
