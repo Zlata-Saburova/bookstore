b383fad3a4eca56a3e6a346e279b7d1cb5d60267
Zlata <zlatasaburova97@gmail.com>
2022-05-29 23:32:05 +0300

    refactor(catr): rename root state

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,10 +1,10 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { routes } from "../routes/routes";
-import { RootStore } from "../store/store";
+import { RootState } from "../store/store";
 
 export const Cart = () => {
-  const { isAuth } = useSelector(({ user }: RootStore) => user);
+  const { isAuth } = useSelector(({ user }: RootState) => user);
 
   if (isAuth) {
     return <div>Cart</div>;
