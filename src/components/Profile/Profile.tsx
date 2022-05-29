fda53ba6f180e695c3962810d7ce1fe19bc24d78
Zlata <zlatasaburova97@gmail.com>
2022-05-29 23:30:31 +0300

    feat(profile): add component and style it

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "../../store/hooks/hooks";
+import { getUserInfo } from "../../store/selectors/userSelectors";
+import { setUser, setUserName } from "../../store/slices/userReducer";
+import {
+  Buttom,
+  FormContainer,
+  Form,
+  Input,
+  Lable,
+  Subtitle,
+  Notification,
+} from "./styles";
+
+export const Profile = () => {
+  const { register, handleSubmit } = useForm();
+  const { name, email } = useAppSelector(getUserInfo);
+  const dispatch = useDispatch();
+  const [isChange, setIsChange] = useState(false);
+
+  const onSubmit = (data: any) => {
+    dispatch(setUserName(data.name));
+    dispatch(setUser(data.email));
+    setIsChange(true);
+  };
+
+  return (
+    <>
+      <Subtitle>Profile</Subtitle>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <FormContainer>
+          <label>
+            <Lable>Name</Lable>
+            <Input type="text" placeholder={name} {...register("name")} />
+          </label>
+          <label>
+            <Lable>Email</Lable>
+            <Input placeholder={email} type="email" {...register("email")} />
+          </label>
+        </FormContainer>
+        <Buttom type="submit">Save changes</Buttom>
+      </Form>
+      {isChange ? <Notification>Changes saved</Notification> : ""}
+    </>
+  );
+};
---
+++ src/components/Profile/styles.ts
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+import { typography } from "../../ui/typography";
+
+export const Subtitle = styled.h3`
+  margin-bottom: 16px;
+
+  ${typography.H3}
+`;
+
+export const Input = styled.input`
+  width: 544px;
+  padding-left: 20px;
+  padding-top: 11px;
+  padding-bottom: 11px;
+  margin-bottom: 40px;
+
+  ${typography.input}
+  border: 1px solid rgb(231, 231, 231);
+
+  &::placeholder {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 32px;
+  }
+`;
+
+export const Lable = styled.p`
+  margin-bottom: 10px;
+
+  ${typography.body}
+
+  cursor: pointer;
+`;
+
+export const Form = styled.form`
+  margin-bottom: 40px;
+`;
+
+export const Buttom = styled.button`
+  display: block;
+  max-width: 480px;
+  width: 100%;
+  margin-left: auto;
+  padding-top: 16px;
+  padding-bottom: 16px;
+
+  ${typography.button}
+  color: rgb(255, 255, 255);
+  text-transform: uppercase;
+
+  background-color: rgb(13, 12, 29);
+
+  transition: all 0.3s;
+
+  &:hover {
+    font-size: 26px;
+  }
+  &:active {
+    font-size: 24px;
+  }
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 40px;
+
+  border-bottom: 1px solid rgb(231, 231, 231);
+`;
+
+export const ErrorMassage = styled.div`
+  max-width: 300px;
+  width: 100%;
+  margin: 0 auto;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin-bottom: 40px;
+  text-align: center;
+
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 32px;
+  color: rgb(255, 0, 0);
+
+  background-color: rgb(241, 218, 196);
+`;
+
+export const Notification = styled(ErrorMassage)`
+  color: rgb(0, 0, 0);
+`;
