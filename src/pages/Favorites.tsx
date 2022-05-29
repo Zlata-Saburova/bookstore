e4966a5ec969375736f8b483c76b379d4e130c9d
Zlata <zlatasaburova97@gmail.com>
2022-05-30 01:08:39 +0300

    feat(favorites): add components to the page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,13 +1,36 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import Slider from "react-slick";
+import { BookSlider } from "../components/BookSlider/BookSlider";
+import { List } from "../components/List/List";
+import { Title } from "../components/Title/Title";
 import { routes } from "../routes/routes";
+import { useAppSelector } from "../store/hooks/hooks";
+import { getUserInfo } from "../store/selectors/userSelectors";
 import { RootState } from "../store/store";
 
 export const Favorites = () => {
   const { isAuth } = useSelector(({ user }: RootState) => user);
+  const { favorites } = useAppSelector(getUserInfo);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    centerPadding: "60px",
+    centerMode: true,
+  };
 
   if (isAuth) {
-    return <div>Favorites</div>;
+    return (
+      <>
+        <Title>Favorites</Title>
+        <List books={favorites}></List>
+        <BookSlider settings={settings} />
+      </>
+    );
   }
 
   return <Navigate to={routes.SIGN_IN} />;
