ed486d5bf0b7ea59ac2673dc2e9c3760bd2bac18
Zlata <zlatasaburova97@gmail.com>
2022-05-30 01:07:04 +0300

    feat(book slider): make settings as props

--- src/components/BookSlider/BookSlider.tsx
+++ src/components/BookSlider/BookSlider.tsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import Slider from "react-slick";
-import { INewBookApi } from "../../services/types";
+import { bookApi } from "../../services/bookService";
+import { INewBookApi, INewBooksApi } from "../../services/types";
 import {
   SlideContainer,
   CustomSlide,
@@ -10,24 +12,37 @@ import {
 } from "./styles";
 
 interface IProps {
-  books: INewBookApi[];
+  settings: {
+    dots: boolean;
+    infinite: boolean;
+    speed: number;
+    slidesToShow: number;
+    slidesToScroll: number;
+    centerMode: boolean;
+    className?: string;
+    centerPadding?: string;
+  };
 }
 
-export const BookSlider = ({ books }: IProps) => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+export const BookSlider = ({ settings }: IProps) => {
+  const [newBooks, setNewBooks] = useState<INewBooksApi>({
+    books: [],
+    error: "",
+    total: "",
+  });
+
+  useEffect(() => {
+    bookApi.getNewBooks().then((books) => {
+      setNewBooks(books);
+    });
+  }, []);
 
   return (
     <div>
       <Slider {...settings}>
-        {books.slice(0, 3).map((book) => {
+        {newBooks.books.slice(0, 5).map((book) => {
           return (
-            <CustomSlide key={book.isbn13} whileHover={{ scale: 1.05 }}>
+            <CustomSlide key={book.isbn13} whileHover={{ scale: 1.03 }}>
               <SlideContainer>
                 <img src={book.image} alt={book.title} />
                 <div>
@@ -40,7 +55,7 @@ export const BookSlider = ({ books }: IProps) => {
             </CustomSlide>
           );
         })}
-        <CustomSlide>
+        <CustomSlide whileHover={{ scale: 1.03 }}>
           <LastSlideContainer>
             <LastSlideText>Watch all new books</LastSlideText>
           </LastSlideContainer>
