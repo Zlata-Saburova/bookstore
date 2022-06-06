f5cfa01aa5185d3fc29c3d372e9e5ac4fda747ae
Zlata <zlatasaburova97@gmail.com>
2022-06-06 23:20:36 +0300

    refacor(cart): add use of cart slice

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -4,18 +4,18 @@ import { CartList } from "../components/CartList/CartList";
 import { Title } from "../components/Title/Title";
 import { routes } from "../routes/routes";
 import { useAppSelector } from "../store/hooks/hooks";
-import { getUserInfo } from "../store/selectors/userSelectors";
+import { getCart } from "../store/selectors/userSelectors";
 import { RootState } from "../store/store";
 
 export const Cart = () => {
   const { isAuth } = useSelector(({ user }: RootState) => user);
-  const { cart } = useAppSelector(getUserInfo);
+  const { cartItems } = useAppSelector(getCart);
 
   if (isAuth) {
     return (
       <>
         <Title>Your Cart</Title>
-        <CartList books={cart}/>
+        <CartList books={cartItems} />
       </>
     );
   }
